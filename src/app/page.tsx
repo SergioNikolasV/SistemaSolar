6bd7627f4fd9b693221bf4df3034150aab48f803
SergioNikolasV <sergiovanegasmv@gmail.com>
2025-09-30 09:38:58 -0500

    Implementaciones sin DataBase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,16 +17,20 @@ export default function Home() {
   }, [timePer]);
 
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1 className="text-4xl sm:text-5xl font-extrabold text-center sm:text-left">
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 bg-slate-950 text-slate-200">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center w-full">
+        <h1 className="text-4xl sm:text-5xl font-extrabold text-center">
           Welcome to{" Solar System Analizer"}
         </h1>
-        <p className="text-lg sm:text-xl text-center sm:text-left max-w-2xl">
+        <p className="text-lg sm:text-xl text-center max-w-2xl">
           This is a web application that allows you to analyze and visualize a particular Solar System.
         </p>
-        <div className="grid grid-cols-2 gap-8 w-full">
-          <div className="flex flex-col gap-4 items-center">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 w-full items-center">
+          <div className="md:col-span-2">
+            <SolarSystem day={day} />
+          </div>
+          <div className="flex flex-col gap-4 items-center justify-center">
+            <label htmlFor="timePer" className="mb-2">Velocidad de simulación (ms por día)</label>
             <Slider
                 id="timePer"
                 min={10}
@@ -34,13 +38,11 @@ export default function Home() {
                 step={10}
                 value={[timePer]}
                 onValueChange={(value) => setTimePer(value[0])}
+                className="w-4/5"
               />
-              <span className="mt-2 text-sm">{timePer} ms</span>
-          </div>  
-          <div className="flex flex-col justify-center">
-            <SolarSystem day={day} />
-            <label htmlFor="timePer" className="mb-2">Velocidad de simulación (ms por día)</label>
+            <span className="mt-2 text-sm">{timePer} ms</span>
           </div>
+
         </div>
       </main>
     </div>
--- src/components/SolarSystem.tsx
+++ src/components/SolarSystem.tsx
@@ -1,3 +1,6 @@
+"use client";
+import { useState, useEffect } from "react";
+
 const planets = [
   { name: "Ferengi", radius: 500, speed: -1, color: "hsl(215 20.2% 65.1%)" },
   { name: "Vulcano", radius: 1000, speed: 5, color: "hsl(24.6 95% 53.1%)" },
@@ -12,20 +15,82 @@ interface PlanetPosition {
   color: string;
   x: number;
   y: number;
+  angleRad: number;
+}
+
+interface rect {
+  x1: number;
+  x2: number
 }
 
 interface SolarSystemProps {
   day: number;
 }
 
+let lineColor: string;
+
 export default function SolarSystem({ day }: SolarSystemProps) {
-  // Posicion (x,y). 
+  
+  const [alignSunT, setAlignSun] = useState(0);
+  const [alignT, setAlign] = useState(0);
+  const [sunInT, setSunInT] = useState(0);
+
   const planetPositions: PlanetPosition[] = planets.map(planet => {
-    const angleRad = (day * planet.speed * Math.PI) / 180;
-    const x = SVG_SIZE / 2 + planet.radius * Math.cos(angleRad);
-    const y = SVG_SIZE / 2 + planet.radius * Math.sin(angleRad);
-    return { ...planet, x, y };
+    const angle = ((day * planet.speed * Math.PI) / 180);
+    const angleRad = Math.abs((angle)%(Math.PI));
+    const x = SVG_SIZE / 2 + planet.radius * Math.cos(angle);
+    const y = SVG_SIZE / 2 + planet.radius * Math.sin(angle);
+    return { ...planet, x, y, angleRad};
   });
+
+  const arePlanetsAlignedSun  = 
+    Math.abs(planetPositions[0].angleRad - planetPositions[1].angleRad) < 0.05 &&
+    Math.abs(planetPositions[1].angleRad - planetPositions[2].angleRad) < 0.05;
+
+  const x1 = (planetPositions[2].y - planetPositions[0].y) / (planetPositions[2].x - planetPositions[0].x);
+  const x2 = (planetPositions[1].y - planetPositions[0].y) / (planetPositions[1].x - planetPositions[0].x);
+  const arePlanetsAligned: boolean = Math.abs(x1 - x2) < 0.15 && !arePlanetsAlignedSun;
+
+  const sign = (p: {x: number, y: number}, pA: PlanetPosition, pB: PlanetPosition) => {
+    return (pB.x - pA.x) * (p.y - pA.y) - (pB.y - pA.y) * (p.x - pA.x);
+  };
+
+  const d1 = sign({x: SVG_SIZE / 2, y: SVG_SIZE / 2}, planetPositions[0], planetPositions[1]);
+  const d2 = sign({x: SVG_SIZE / 2, y: SVG_SIZE / 2}, planetPositions[1], planetPositions[2]);
+  const d3 = sign({x: SVG_SIZE / 2, y: SVG_SIZE / 2}, planetPositions[2], planetPositions[0]);
+
+  const negativeT= (d1 < 0) || (d2 < 0) || (d3 < 0);
+  const positiveT= (d1 > 0) || (d2 > 0) || (d3 > 0);
+
+
+  const isSunInT = !( negativeT && positiveT );
+  
+  useEffect(() => {
+    if (arePlanetsAlignedSun) {
+      setAlignSun(prevCount => prevCount + 1); 
+    } else if (arePlanetsAligned) {
+      setAlign(prevCount => prevCount + 1);
+    } else if (isSunInT) {
+      setSunInT(prevCount => prevCount + 1); 
+    } else {
+    }
+  }, [day, arePlanetsAligned, arePlanetsAlignedSun, isSunInT]);
+
+  if (arePlanetsAlignedSun) {
+    lineColor = "hsl(48, 100%, 50%)";
+  } else if (arePlanetsAligned) {
+    lineColor = "hsla(0, 66%, 53%, 1.00)";
+  } else if (isSunInT) {
+    lineColor = "hsla(155, 52%, 56%, 1.00)";
+  } else {
+    lineColor = "hsla(214, 49%, 63%, 1.00)";  
+  }
+
+  const aSide = Math.sqrt(Math.pow((planetPositions[0].x)-(planetPositions[1].x), 2) + Math.pow((planetPositions[0].y)-(planetPositions[1].y), 2))
+  const bSide = Math.sqrt(Math.pow((planetPositions[1].x)-(planetPositions[2].x), 2) + Math.pow((planetPositions[1].y)-(planetPositions[2].y), 2))
+  const cSide = Math.sqrt(Math.pow((planetPositions[2].x)-(planetPositions[0].x), 2) + Math.pow((planetPositions[2].y)-(planetPositions[0].y), 2))
+  const perimeter = (aSide + bSide + cSide);
+
 
   return (
     <div className="w-full max-w-4xl mx-auto p-4">
@@ -42,10 +107,52 @@ export default function SolarSystem({ day }: SolarSystemProps) {
           {/*Sol*/}
           <circle cx={SVG_SIZE / 2} cy={SVG_SIZE / 2} r={250} fill="yellow" />
 
+          {/*LineasSol*/}
+          {planetPositions.map(p => (
+            <line
+              key={`line-${p.name}`}
+              x1={SVG_SIZE / 2}
+              y1={SVG_SIZE / 2}
+              x2={p.x}
+              y2={p.y}
+              stroke={lineColor}
+              strokeWidth="2"
+            />
+          ))}
+
           {/*Planetas*/}
           {planetPositions.map(p => (
             <circle key={p.name} cx={p.x} cy={p.y} r={80} fill={p.color} />
           ))}
+
+          {/*LineasTriangulo*/}
+          <line
+            key={`lineTriangle1-${planetPositions[0].name} ${planetPositions[1].name}`}
+            x1= {planetPositions[0].x}
+            y1= {planetPositions[0].y}
+            x2= {planetPositions[1].x}
+            y2= {planetPositions[1].y}
+            stroke={lineColor}
+            strokeWidth="5"
+          />
+          <line
+            key={`lineTriangle2-${planetPositions[1].name} ${planetPositions[2].name}`}
+            x1= {planetPositions[1].x}
+            y1= {planetPositions[1].y}
+            x2= {planetPositions[2].x}
+            y2= {planetPositions[2].y}
+            stroke={lineColor}
+            strokeWidth="5"
+          />
+          <line
+            key={`lineTriangle3-${planetPositions[2].name} ${planetPositions[0].name}`}
+            x1= {planetPositions[2].x}
+            y1= {planetPositions[2].y}
+            x2= {planetPositions[0].x}
+            y2= {planetPositions[0].y}
+            stroke={lineColor}
+            strokeWidth="5"
+          />
         </svg>
       </div>
 
@@ -59,6 +166,21 @@ export default function SolarSystem({ day }: SolarSystemProps) {
             </li>
           ))}
         </ul>
+        {arePlanetsAlignedSun && (
+          <p className="mt-2 text-yellow-400 font-bold">
+            Clima: ¡Alineación planetaria con el Sol! Sequia por <strong>{alignSunT}</strong> vez.
+          </p>
+        )}
+        {arePlanetsAligned && (
+          <p className="mt-2 text-red-400 font-bold">
+            Clima: ¡Alineación planetaria con sin el Sol! Buen clima <strong>{alignT}</strong> vez.
+          </p>
+        )}
+        {isSunInT && (
+          <p className="mt-2 text-yellow-400 font-bold">
+            Clima: ¡Sol dentro de triangulo! Tormentas por <strong>{sunInT}</strong> vez de magnitud <strong>{perimeter}</strong> .
+          </p>
+        )}
       </div>
     </div>
   );
